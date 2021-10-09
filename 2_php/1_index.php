c33c36f6e0b6558ee78dec16e1eab5bcb18b214a
Herman Kudria <h.kudrya@hotmail.com>
2021-10-09 15:32:48 +0200

    Add files via upload
    aktualizacja

--- 2_php/1_index.php
+++ 2_php/1_index.php
@@ -2,9 +2,18 @@
 <html lang="pl" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Strona glówna</title>
+    <title>Strona główna</title>
   </head>
   <body>
     <p>Strona główna</p>
+    <?php
+  $name='Janusz';
+  $surname="Surname";
+    echo 'imię: $name';
+    echo "<br>imię: $name<hr>";
+
+
+
+     ?>
   </body>
 </html>
