f7357d949063f2d4a4a769b0a65a928faa61d4d2
Herman Kudria <h.kudrya@hotmail.com>
2021-10-15 19:19:48 +0200

    php_marathon 3

--- own_expiriens/lesson1/index.php
+++ own_expiriens/lesson1/index.php
@@ -5,7 +5,40 @@
     <title>PHP - marathon</title>
   </head>
   <body>
+    <h1>Function</h1>
     <?php
+    const PI = 3.14;
+
+//function void
+    function areaCircleEcho($r){
+      echo PI * $r * $r , '<br>';
+    }
+
+    areaCircleEcho(5);
+
+//function T
+    function areaCircle($r){
+      return PI * $r * $r;
+    }
+
+    echo areaCircle(5) - areaCircle(2);
+
+    echo "<br>";
+    echo "<br>";
+
+//while - max time 120s
+
+    $i = 1;
+    while ($i <= 10) {
+      echo $i++;
+    }
+
+//for
+
+    for ($i=0; $i < 10 ; $i++) {
+      echo $i++;
+    }
+
 
      ?>
   </body>
